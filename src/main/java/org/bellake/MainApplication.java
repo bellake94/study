235dc5641e1d154de1f59bc7150f6c96d702dfb0
bellake <vmfhrltk94@naver.com>
2022-12-04 11:22:59 +0900

    Spring boot 설정 적용

---
+++ src/main/java/org/bellake/MainApplication.java
@@ -0,0 +1,10 @@
+package org.bellake;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class);
+    }
+}
